ce0a649f444534ed5492bc2891c29a58c4d55901
Tan Duong Thanh <nobudy55@gmail.com>
2019-11-23 21:20:39 +0700

    add prettier config and set vuetify rule for tsconfig.json

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 95,
+  singleQuote: true,
+  semi: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  trailingComma: 'es5',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+};
